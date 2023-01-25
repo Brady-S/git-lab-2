b1d5d2b247178799b90255189d5747377102b8e5
BradyStoker <bs998321@ohio.edu>
2023-01-24 20:09:09 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Brady Stoker>
+ *        Date: <01/24/23>
+ * Description: Lab 2 Code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
